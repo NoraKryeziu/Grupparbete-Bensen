cda81b1cb1663fc15b898f2adf924957617d49e8
CalinJilavu <23583666+c8j@users.noreply.github.com>
2025-04-08 09:18:26 +0200

    Implement post and delete functions

--- webdev-uppgift-main/src/lib/utilities/httpClient.js
+++ webdev-uppgift-main/src/lib/utilities/httpClient.js
@@ -1,4 +1,4 @@
-import { config } from "../../data/config.js";
+import { config } from "../../data/config.js"
 
 export const get = async (endpoint) => {
     try {
@@ -12,9 +12,50 @@ export const get = async (endpoint) => {
         if (response.ok) {
             return await response.json()
         } else {
-            throw new Error('Något gick fel')
+            throw new Error(`Något gick fel: ${response.status} - ${response.statusText}`)
         }
     } catch (error) {
-        console.error(error);
+        console.error(error)
+    }
+}
+
+export const post = async (endpoint, object) => {
+    try {
+        const response = await fetch(`${config.apiUrl}/${endpoint}`, {
+            method: 'POST',
+            headers: {
+                'x-apikey': config.apiKey,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(object)
+        })
+
+        if (response.ok) {
+            return await response.json()
+        } else {
+            throw new Error(`Något gick fel: ${response.status} - ${response.statusText}`)
+        }
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+export const remove = async (endpoint) => {
+    try {
+        const response = await fetch(`${config.apiUrl}/${endpoint}`, {
+            method: 'DELETE',
+            headers: {
+                'x-apikey': config.apiKey,
+                'Content-Type': 'application/json',
+            }
+        })
+
+        if (response.ok) {
+            return await response.json()
+        } else {
+            throw new Error(`Något gick fel: ${response.status} - ${response.statusText}`)
+        }
+    } catch (error) {
+        console.error(error.message)
     }
 }
